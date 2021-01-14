4caac8101ebf592cace582d2da2ddedf6e53b9f3
treguv <vladislavtregubov00@gmail.com>
2021-01-14 00:19:04 -0800

    Removed the calls to AWS s3 to prepare switch to cloudinary

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,13 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes.js");
 const postRoutes = require("./post-routes");
 const commentRoutes = require("./comment-routes");
-const uploadRoutes = require("./aws-routes.js");
+// const uploadRoutes = require("./aws-routes.js");
 
 // prefixes routes
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use("/comments", commentRoutes);
 //aws upload image routes
-router.use("/upload", uploadRoutes);
+// router.use("/upload", uploadRoutes);
 
 module.exports = router;
